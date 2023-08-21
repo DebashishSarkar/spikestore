e18bec210271cd4da7b28a582e3d632a16683f5a
Debashish Sarkar <dsarkar@lutron.com>
2023-08-21 15:08:59 +0530

    add 2 numbers by post

--- apim-001/Add.cs
+++ apim-001/Add.cs
@@ -14,22 +14,13 @@ namespace Calculator
     {
         [FunctionName("Add")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            dynamic data = JsonConvert.DeserializeObject(requestBody);            
+            var sum= data?.a + data?.b;                                    
+            return new OkObjectResult($"Sum: {sum}.");
         }
     }
 }
